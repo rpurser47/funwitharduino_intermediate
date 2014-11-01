4d8f1da1bc02110795cac04e76f9cca9ce7f06fc
Rob Purser <rob@purser.org>
2014-11-01 06:59:59 -0400

    Initial version of class 5 code

---
+++ class_5/FunWithArduinoLib/slblocks.m
@@ -0,0 +1,7 @@
+function blkStruct = slblocks
+  % Specify that the product should appear in the library browser
+  % and be cached in its repository
+  Browser.Library = 'FunWithArduinoLib';
+  Browser.Name    = 'FunWithArduinoLib';
+  blkStruct.Browser = Browser;
+  
